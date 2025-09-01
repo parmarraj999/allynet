7b208d8145562bbe3ee98dabb09a24f86bb9f96d
BravoCoders <bravocodersunited@gmail.com>
2025-09-01 21:33:40 +0530

    feat: add LoginPage route in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './page/landing-page/landingPage';
+import LoginPage from './login-page/loginPage';
 
 function App() {
   return (
     <div className="App">
-        <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage/>} />
+          <Route path='/login' element={
+            <LoginPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
